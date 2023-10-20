960a2dac82b79b646b02a5ffea991f6651d7bc72
AndresBarretos24 <omarbarreto@quadi.co>
2023-10-20 11:13:03 -0500

    adding socket config

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
+const http = require('http');
+const { Server } = require('socket.io');
 
-app.use(express.static('public'))
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:4321"],
+        methods: ["GET", "POST"]
+    }
+}); // Asocia socket.io con el servidor
+
+let contador = 0;
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.emit('counterUpdated', contador);
+
+    socket.on('clickedButton', () => {
+        contador++;
+        io.sockets.emit('counterUpdated', contador);
+    });
+});
+
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     res.redirect('/');
-})
+});
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+server.listen(port, () => {
+    console.log(`App listening on portttt ${port}`);
 })
